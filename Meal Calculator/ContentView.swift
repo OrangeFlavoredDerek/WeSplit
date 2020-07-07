291efd3c6721878c522e4bee86148241dcf4abe3
OrangeFlavoredDerek <derekchan0407@gmail.com>
2020-07-07 16:07:40 +0800

    Update ContentView.swift

--- Meal Calculator/ContentView.swift
+++ Meal Calculator/ContentView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson:Double{
+        let orderAmount = Double(checkAmount) ?? 0
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        
+        let tipValue = orderAmount/100 * tipSelection
+        let grandTotal = tipValue + orderAmount
+        let amountPerPerson = grandTotal/peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("总金额", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("人数", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0)人")
+                        }
+                    }
+                }
+                
+                Section(header: Text("选择小费比例")
+                    .font(.system(size: 15.5))
+                    .foregroundColor(.black)){
+                    Picker("小费比例", selection: $tipPercentage){
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section{
+                    Text("每人\(totalPerPerson, specifier: "%.2f")元")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+            .font(.headline)
+        }
     }
 }
 
