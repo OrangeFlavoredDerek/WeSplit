479fe528530a420db297163b490249846157b1f7
陈峻琦 <chenjunqi07@icloud.com>
2020-07-07 15:54:23 +0800

    Initial Commit

---
+++ Meal Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meal Calculator
+//
+//  Created by 陈峻琦 on 2020/7/7.
+//  Copyright © 2020 derekchan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
